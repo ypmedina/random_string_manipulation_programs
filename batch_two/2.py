1c0f8f52aceb6648f55647273b40e1df194c9e6e
unknown <yaelmedina2031@gmail.com>
2025-03-28 21:18:14 +0800

    Made the program take inputs and tinkering on the main code

---
+++ batch_two/2.py
@@ -0,0 +1,10 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter.
+#Create a program that do the same functionality without using removeprefix() function.
+
+#To make it easier, just ask the user what is the prefix in the word and remove it
+
+user_input = input("Please enter a word")
+user_prefix = input("What is the prefix used in the word? If none, please press enter")
+
+if user_prefix and user_input.startswith(user_prefix):
+    
