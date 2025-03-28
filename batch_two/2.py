925c6cb5b5ec7d02537b4b0a9e659b5885506793
unknown <yaelmedina2031@gmail.com>
2025-03-28 21:20:54 +0800

    Made the main code for the program

--- batch_two/2.py
+++ batch_two/2.py
@@ -3,8 +3,12 @@
 
 #To make it easier, just ask the user what is the prefix in the word and remove it
 
-user_input = input("Please enter a word")
-user_prefix = input("What is the prefix used in the word? If none, please press enter")
+user_input = input("Please enter a word: ")
+user_prefix = input("What is the prefix used in the word? If none, please press enter: ")
 
 if user_prefix and user_input.startswith(user_prefix):
-    
+    no_prefix = user_input[len(user_prefix):]
+else:
+    no_prefix = user_input
+
+print(no_prefix)
