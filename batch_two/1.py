943e9300026a38fbd869e87d0b1248a26c914d3a
unknown <yaelmedina2031@gmail.com>
2025-03-28 20:44:20 +0800

    Made an if statement. Still trying to figure out what to do

--- batch_two/1.py
+++ batch_two/1.py
@@ -1,2 +1,8 @@
 #Prog01. ltrip() remove the space characters at the beginning of the string. Create a program that do the
 #same functionality without using lstrip() function.
+
+#Create an if statement that reads spaces and moves to the right while making it stop when it reads a letter
+
+sentence = "   No spaces here"
+if sentence.startswith(" "):
+
