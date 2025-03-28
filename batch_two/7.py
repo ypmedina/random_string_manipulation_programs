8a381765e16b14c038a4a9ec14d947fd05a608ee
unknown <yaelmedina2031@gmail.com>
2025-03-28 22:22:42 +0800

    made the program take user input

---
+++ batch_two/7.py
@@ -0,0 +1,6 @@
+#Prog07. center() add space characters at the beginning and at the end of the string to print the
+#string at the center. Create a program that do the same functionality without using center() function.
+
+#idk man
+
+user_inp = input("Please enter a word or a sentence: ")
